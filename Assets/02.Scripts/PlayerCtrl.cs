71abf04bf47be523e71154f595c441710506a57d
Lee JaeHyun <myleje@gmail.com>
2021-03-12 15:39:44 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -6,7 +6,8 @@ public class PlayerCtrl : MonoBehaviour
 {
     [SerializeField]
     private Transform tr;
-    public float moveSpeed = 5.0f;
+    public float moveSpeed = 10.0f;
+    public float turnSpeed = 100.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,12 +20,14 @@ public class PlayerCtrl : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal"); // -1.0f ~ 0.0f ~ +1.0f
         float v = Input.GetAxis("Vertical");   // -1.0f ~ 0.0f ~ +1.0f
+        float r = Input.GetAxis("Mouse X");
 
         //Debug.Log("h=" + h + " v=" + v); //Debug.LogFormat()
         Debug.Log($"h={h} v={v}");  //문자열 치환
         
         Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
         tr.Translate(moveDir.normalized * Time.deltaTime * moveSpeed);
+        tr.Rotate(Vector3.up * Time.deltaTime * r * turnSpeed);
 
         /* 
             정규화벡터(Normalized vector), 단위벡터(Unit Vector)
