7b53b5f4c20e13efd9e7b7a97ffeeef25250ffa8
Lee JaeHyun <myleje@gmail.com>
2021-03-19 16:02:21 +0900

    .

--- Assets/02.Scripts/MonsterCtrl.cs
+++ Assets/02.Scripts/MonsterCtrl.cs
@@ -7,12 +7,16 @@ public class MonsterCtrl : MonoBehaviour
 {
     private NavMeshAgent agent;
     private Transform playerTr;
+    private Animator anim;
 
     void Start()
     {
+        anim = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
         playerTr = GameObject.Find("Player").GetComponent<Transform>(); //하이러키 뷰의 게임오브젝트의 이름으로 검색
         playerTr = GameObject.FindGameObjectWithTag("PLAYER").GetComponent<Transform>(); //게임오브젝트의 태그명으로 검색
+
+        anim.SetBool("IsTrace", true);
     }
 
     void Update()
